7e1c9137aa9a90e80d90d650462dba891edca8e8
Kangdora <ioprt123@gmail.com>
2025-02-14 07:01:08 +0900

    Update: 로또 가격과 최대 등수 상수 추가

--- src/main/java/domain/LottoConstants.java
+++ src/main/java/domain/LottoConstants.java
@@ -1,8 +1,6 @@
 package domain;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -10,19 +8,10 @@ public class LottoConstants {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
     public static final int LOTTO_LENGTH = 6;
+    public static final int LOTTO_PRICE = 1000;
+    public static final int MAX_GRADE = 4;
 
     public static final List<Integer> LOTTO_RANGES = IntStream.range(MIN_NUMBER, MAX_NUMBER)
             .boxed()
             .collect(Collectors.toList());
-
-    public static final Map<Integer, Integer> PRIZE_MAP = createPrizeMap();
-
-    private static Map<Integer, Integer> createPrizeMap() {
-        Map<Integer, Integer> prizeMap = new HashMap<>();
-        prizeMap.put(3, 5000);
-        prizeMap.put(4, 50000);
-        prizeMap.put(5, 1500000);
-        prizeMap.put(6, 2000000000);
-        return prizeMap;
-    }
 }
