8566b2f7fedec0d127d0771561652f4ecfc70921
Kangdora <ioprt123@gmail.com>
2025-02-14 16:55:43 +0900

    Refactor: 캬캬캬 record class로 리팩터링!!!

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,30 +1,14 @@
 package domain;
 
+import java.util.Collections;
 import java.util.List;
 
-public class Lotto {
-    private final List<Integer> numbers;
-
-    public Lotto(List<Integer> numbers){
+public record Lotto(List<Integer> numbers) {
+    public Lotto {
         if (numbers.size() != 6) {
             throw new ArithmeticException("로또 번호는 6개여야 합니다.");
         }
-        this.numbers = numbers;
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-
-    public int countMatchingnumber(List<Integer> winningNumbers) {
-        return Long.valueOf(numbers.stream().filter(winningNumbers::contains).count()).intValue();
-    }
-
-    public boolean haveBonusNumber(int bonusNumber) {
-        return numbers.contains(bonusNumber);
-    }
-
-    public List<Integer> getLottoList(){
-        return numbers;
+        numbers = List.copyOf(numbers);
+        Collections.sort(numbers);
     }
 }
